89429a12ba3365647d1661c594e02825e09ddc78
ComIT-Ozzie <oz.neher+comit@gmail.com>
2019-10-15 19:43:21 -0600

    Completed lesson 19

---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,19 @@
+const monthOfBirth = "August";
+const firstName = "Ozzie";
+const lastName = "Neher";
+
+let city;
+let age;
+let nextCity;
+
+city = "Saskatoon";
+age = 55;
+nextCity = city;
+
+console.log("Month of birth: ", monthOfBirth);
+console.log("First name: ", firstName);
+console.log("Last name: ", lastName);
+console.log("City: ", city);
+console.log("Age: ", age);
+console.log("Next city: ", nextCity);
+console.log("Full name: ", `${firstName} ${lastName}`);
